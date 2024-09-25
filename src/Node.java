dccda4e5e6472cdef4c58ae6056357fbbc78837f
mike iaccarino <iaccmj21@wfu.edu>
2024-09-25 14:16:34 -0400

    version 2

--- src/Node.java
+++ src/Node.java
@@ -1,22 +1,64 @@
+interface binNode<E>{
+    public E value();
+    public void setValue(E v);
+
+    public binNode<E> left();
+    public binNode<E> right();
+
+    public boolean isLeaf();
+}
+
 // Implement the constructor
+public class Node <E extends Comparable<? super E>> implements binNode<E> {
+    E element;
+    Node<E> left;
+    Node<E> right;
+
+    Node() {
+        left = right = null;
+    }
+    Node(E val){
+        if (val == null){
+            throw new NullPointerException();
+        }
+        left = right = null;
+        element = val;
+    }
+    Node(E val, Node<E> l, Node<E> r){
+        element = val;
+        left = l;
+        right = r;
+    }
 
 
-// Implement the setElement method
+    // Implement the setElement method
+    @Override
+    public E value() {
+        return element;
+    }
+
+    public void setValue(E val) {element = val; }
 
 
-// Implement the setLeft method
+    // Setters and getters: left
+    public Node<E> left() {return left;}
+    public void setLeft(Node<E> p) {left = p;}
+
+    // setters and getters: right
+    public Node<E> right() {return right;}
+    public void setRight(Node<E> p) {this.right = p;}
 
 
-// Implement the setRight method
+    // Implement the isLeaf method
+
+    public boolean isLeaf() {
+        return (left == null) && (right == null);
+    }
+
+    //compareTO
 
 
-// Implement the getLeft method
 
 
-// Implement the getRight method
 
-
-// Implement the getElement method
-
-
-// Implement the isLeaf method
+}
--- src/Proj1.java
+++ src/Proj1.java
@@ -1,7 +1,8 @@
 import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class Proj1 {
-    public static void main(String[] args) throws FileNotFoundException{
+    public static void main(String[] args) throws IOException {
         if(args.length != 1){
             System.err.println("Argument count is invalid: " + args.length);
             System.exit(0);
